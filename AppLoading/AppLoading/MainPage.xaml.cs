8dcfd9b08b2411cf160b0972a5bba2fb9926a85f
Thiago Chagas <thiago.chagas@blnet.com>
2017-10-13 11:37:53 -0300

    Acitivity indicator  Xamarin forms OK

--- AppLoading/AppLoading/MainPage.xaml.cs
+++ AppLoading/AppLoading/MainPage.xaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace AppLoading
 {
     public partial class MainPage : ContentPage
     {
+        private MainViewModel ViewModel
+        {
+            get { return BindingContext as MainViewModel; }
+            set { BindingContext = value; }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            ViewModel = new MainViewModel();
+            BindingContext = ViewModel;
+
         }
     }
 }
---
+++ AppLoading/AppLoading/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppLoading
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand DispararEventoCommand { get; private set; }
+
+        bool _IsLoading;
+
+        public bool IsLoading
+        {
+            get { return _IsLoading; }
+            set
+            {
+                SetValue(ref _IsLoading, value);
+                OnPropertyChanged(nameof(_IsLoading));
+            }
+        }
+
+        public MainViewModel()
+        {
+            DispararEventoCommand = new Command(DispararEvento);
+            IsLoading = false;
+        }
+
+        async void DispararEvento()
+        {
+            IsLoading = true;
+
+            await Task.Delay(6000);
+
+            IsLoading = false;
+        }
+    }
+}
